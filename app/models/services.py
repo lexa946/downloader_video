3d4385af77aedffbc51208fdc149decd9c461573
APozhar <apozhar@inno.tech>
2025-08-22 11:13:54 +0300

    Добавил поддержку ВК клипов

--- app/models/services.py
+++ app/models/services.py
@@ -28,7 +28,7 @@ class InstagramVideoService(VideoServiceBase):
 
 class VkVideoService(VideoServiceBase):
     name = "VK"
-    key_words = ['vkvideo', 'vk.com/video', 'vk.com/club']
+    key_words = ['vkvideo', 'vk.com/video', 'vk.com/club', 'vk.com/clip']
     parser = VkParser
 
 
